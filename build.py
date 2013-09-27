6e96cb93ed9e6c6d4a20f22d93ca7e99b3144233
Paul Durrant <paul.durrant@citrix.com>
2013-09-27 13:55:49 +0100

    Update build to reflect version
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- build.py
+++ build.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
     driver = 'xennet'
 
     os.environ['MAJOR_VERSION'] = '7'
-    os.environ['MINOR_VERSION'] = '1'
+    os.environ['MINOR_VERSION'] = '2'
     os.environ['MICRO_VERSION'] = '0'
 
     if 'BUILD_NUMBER' not in os.environ.keys():
