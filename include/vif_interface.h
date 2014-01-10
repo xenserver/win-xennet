88bb88a0fbc9ff96cd47d8e22877f2d591973f4f
Paul Durrant <paul.durrant@citrix.com>
2014-01-10 10:37:59 +0000

    Update to latest xenvif interface
    Signed-off-by: Paul Durrant <paul.durrant@citrix.com>

--- include/vif_interface.h
+++ include/vif_interface.h
@@ -35,8 +35,6 @@
 #include <ifdef.h>
 #include <ethernet.h>
 
-#define MAX_SKB_FRAGS   ((65536/PAGE_SIZE) + 2)
-
 typedef UCHAR   XENVIF_PACKET_STATUS, *PXENVIF_PACKET_STATUS;
 
 #define PACKET_STATUS_INVALID   0
@@ -192,15 +190,15 @@ typedef struct _XENVIF_TRANSMITTER_PACKET_METADATA {
 } XENVIF_TRANSMITTER_PACKET_METADATA, *PXENVIF_TRANSMITTER_PACKET_METADATA;
 
 typedef struct _XENVIF_TRANSMITTER_PACKET_STATISTICS {
-    ULONG   Drop;
-    ULONG   BackendError;
-    ULONG   FrontendError;
-    ULONG   Unicast;
-    ULONG   UnicastBytes;
-    ULONG   Multicast;
-    ULONG   MulticastBytes;
-    ULONG   Broadcast;
-    ULONG   BroadcastBytes;
+    ULONGLONG   Drop;
+    ULONGLONG   BackendError;
+    ULONGLONG   FrontendError;
+    ULONGLONG   Unicast;
+    ULONGLONG   UnicastBytes;
+    ULONGLONG   Multicast;
+    ULONGLONG   MulticastBytes;
+    ULONGLONG   Broadcast;
+    ULONGLONG   BroadcastBytes;
 } XENVIF_TRANSMITTER_PACKET_STATISTICS, *PXENVIF_TRANSMITTER_PACKET_STATISTICS;
 
 typedef struct _XENVIF_PACKET_STATISTICS {
@@ -417,7 +415,7 @@ DEFINE_GUID(GUID_VIF_INTERFACE,
             0x95,
             0xc3);
 
-#define VIF_INTERFACE_VERSION    13
+#define VIF_INTERFACE_VERSION    14
 
 #define VIF_OPERATIONS(_Interface) \
         (PXENVIF_VIF_OPERATIONS *)((ULONG_PTR)(_Interface))
